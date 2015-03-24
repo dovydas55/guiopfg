974711637a900cf67183006afdefd2afb1c6dc2c
Dovydas Stankevicius <dovydas55@hotmail.com>
2015-03-24 01:41:35 +0000

    initialial style update for official project

---
+++ src/app/controllers/HeaderController.js
@@ -0,0 +1,10 @@
+angular.module('GUIOPFG').controller('HeaderController', ['$scope', function($scope) {
+  "use strict"; //strick javascript mode
+  $scope.init = function(){
+
+  };
+
+  //iniliatize the variables after everything has loaded
+  $scope.init();
+
+}]);
